8fca572711824c7e907136f187583ddab89ca6c9
Brandon Parrillas <brandonps.dev@gmail.com>
2024-06-03 21:41:50 -0600

    lector funcionando

--- app/src/main/java/com/parrillas/proyectoinnovacionpdm2024_gt1_grupo2/ui/scanner/ScannerFragment.kt
+++ app/src/main/java/com/parrillas/proyectoinnovacionpdm2024_gt1_grupo2/ui/scanner/ScannerFragment.kt
@@ -1,26 +1,36 @@
 package com.parrillas.proyectoinnovacionpdm2024_gt1_grupo2.ui.scanner
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import com.parrillas.proyectoinnovacionpdm2024_gt1_grupo2.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val CAMERA_REQUEST_CODE = 101
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ScannerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ScannerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,7 @@ class ScannerFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        setupPermissions()
     }
 
     override fun onCreateView(
@@ -35,7 +46,95 @@ class ScannerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+        val view = inflater.inflate(R.layout.fragment_scanner, container, false)
+        initComponents(view)
+        initUI()
+        initListeners()
+        return view
+    }
+
+    fun initComponents(view: View) {
+        codeScanner = CodeScanner(requireContext(), view.findViewById(R.id.codeScanner))
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+            decodeCallback = DecodeCallback {
+                requireActivity().runOnUiThread {
+                    Toast.makeText(requireContext(), "Scan result: ${it.text}", Toast.LENGTH_LONG)
+                        .show()
+//                    findNavController().navigate(R.id.action_nav_scanner_to_nav_product_detail)
+                }
+            }
+            errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+                requireActivity().runOnUiThread {
+                    Toast.makeText(
+                        requireContext(), "Camera initialization error: ${it.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Log.e("ScannerFragment", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+    }
+
+    fun initUI() {}
+    fun initListeners() {
+        codeScanner.startPreview()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("ScannerFragment", "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        requireContext(),
+                        "You need the camera permission to be able to use this app!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Log.i("ScannerFragment", "Permission has been denied by user")
+                } else {
+                    Log.i("ScannerFragment", "Permission has been granted by user")
+                }
+            }
+        }
     }
 
     companion object {
