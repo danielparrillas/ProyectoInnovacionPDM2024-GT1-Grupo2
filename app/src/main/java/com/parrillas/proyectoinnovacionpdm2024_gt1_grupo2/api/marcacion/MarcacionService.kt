03dc67b204b9188885d4b340f4e05ecf2ec354d9
Brandon Parrillas <brandonps.dev@gmail.com>
2024-06-05 20:53:28 -0600

    correjir la api no devuelve nada

--- app/src/main/java/com/parrillas/proyectoinnovacionpdm2024_gt1_grupo2/api/marcacion/MarcacionService.kt
+++ app/src/main/java/com/parrillas/proyectoinnovacionpdm2024_gt1_grupo2/api/marcacion/MarcacionService.kt
@@ -1,15 +1,13 @@
 package com.parrillas.proyectoinnovacionpdm2024_gt1_grupo2.api.marcacion
 
-import com.parrillas.proyectoinnovacionpdm2024_gt1_grupo2.api.login.LoginRequest
-import com.parrillas.proyectoinnovacionpdm2024_gt1_grupo2.api.login.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface MarcacionService {
     @POST("/qr/entrada")
-    fun marcarEntrada(@Body marcacionRequest: MarcacionRequest): Call<MarcacionResponse>
+    fun marcarEntrada(@Body marcacionRequest: MarcacionRequest): Call<Void>
 
     @POST("/qr/salida")
-    fun marcarSalida(@Body marcacionRequest: MarcacionRequest): Call<MarcacionResponse>
+    fun marcarSalida(@Body marcacionRequest: MarcacionRequest): Call<Void>
 }
